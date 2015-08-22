febe94d4c958bc238c852e63b15ba96a618111f3
zleikgb <zleikgb@gmail.com>
2015-08-22 10:52:33 +0800

    Inversion
    Coursera R Language
    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- funtion (y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinversion <- function(inversion) inv <<- inversion
+    getinversion <- function() inv
+    list(set = set, get = get, setinversion = setinversion, getinversion = getinversion)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinversion()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return inv
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinversion(inv)
+    inv
 }
